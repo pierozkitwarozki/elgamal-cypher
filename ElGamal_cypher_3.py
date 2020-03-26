973ba62ac9d8db78e01cf4fd76be247a6098f4da
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-03-26 03:29:12 +0100

    ElGamal Cypher in python - with comments

--- ElGamal_cypher_3.py
+++ ElGamal_cypher_3.py
@@ -21,16 +21,17 @@ def generate_random_p():
     return p
 
 
-def encode(text):
+def encode(text):  # zamienianie kazdego znaku w ciągu na inta z tablicy ASCII, zeby potem
+    # mozna było wykonywać na nich operacje matematyczne
     for i in text:
-        yield ord(i)
+        yield ord(i)  # yield to taki jakby return tylko dla każdej iteracji w pętli, nie zatrzymuje pętli
 
 
 def mod_exp(base, exp, modulus):
     return pow(base, exp, modulus)  # działanie: base ^ exp % modulus
 
 
-def generate_keys():  # generowanie kluczy
+def generate_keys():  # generowanie kluczy, wykonywanie funkcji po prostu
     p = generate_random_p()
     a = find_primitive(p)
     x = random.randint(1, p - 1)
@@ -39,31 +40,6 @@ def generate_keys():  # generowanie kluczy
     return {'public': (a, p, y),
             'private': x,
             'k_value': k}
-
-
-def encrypt(key, message):
-    encoded_msg = list(encode(message))
-    pairs = []
-    x = 1
-    for item in encoded_msg:
-        y = mod_exp(key['public'][0], key['k_value'], key['public'][1])
-        z = item * (mod_exp(key['public'][2], key['k_value'], key['public'][1]))
-        pairs.append((y, z))
-        x += 1
-    return pairs
-
-
-def decrypt(key, message):
-    x = key['private']
-    p = key['public'][1]
-    decoded_msg = ''
-    for item in message:
-        first = item[0]  # 28
-        second = item[1]  # 9
-        # m = (second * (pow(first, p-1-x)))%p
-        m = (second * (first ** (p - 1 - x))) % p
-        decoded_msg += str(chr(m))
-    return decoded_msg
 
 
 def find_prime_factors(s, n):
@@ -95,14 +71,41 @@ def find_primitive(n):
     return -1
 
 
+def encrypt(key, message):  # metoda szyfrująca
+    encoded_msg = list(encode(message))  # tu zamieniam słowa na ascii
+    pairs = []  # tu bede zapisywał pary zaszyfrowane
+    x = 1
+    for item in encoded_msg:  # dla każdego znaku takie działanie
+        y = mod_exp(key['public'][0], key['k_value'], key['public'][1])
+        z = item * (mod_exp(key['public'][2], key['k_value'], key['public'][1]))
+        pairs.append((y, z))
+        x += 1
+    encrypted_msg_str = ''
+    for i in pairs:  # łączę wszystkie pary w ciag liczb żeby się nie wyświetlało jak lista,
+        encrypted_msg_str += str(i[0]) + str(i[1])
+    return pairs, encrypted_msg_str  # zwracam listę par i ciąg do wyświetlenia
+
+
+def decrypt(key, message):  # no i metoda deszyfrująca
+    x = key['private']
+    p = key['public'][1]
+    decoded_msg = ''
+    for item in message:
+        first = item[0]
+        second = item[1]
+        m = (second * (first ** (p - 1 - x))) % p
+        decoded_msg += str(chr(m))  # zamieniam z powrotem na stringa
+    return decoded_msg
+
+
 keys = generate_keys()
 
-input_text = input('Enter text to encrypt: ')
-encrypted_msg = encrypt(keys, input_text)
+# input_text = input('Enter text to encrypt: ') jak odkomentujesz, i zakomentujesz to niżej,
+# to można wprowadzać tekst z klawiatury
+input_text = 'SzYfRRR'
+encrypted_msg, encrypted_msg_str = encrypt(keys, input_text)
 decrypted_msg = decrypt(keys, encrypted_msg)
 
-for i in encrypted_msg:
-    encrypted_msg = ''.join(str(i[0]) + str(i[1]))
-
-print(f'Encrypted message: {encrypted_msg}')
+print(f'You entered message: {input_text}')
+print(f'Encrypted message: {encrypted_msg_str}')
 print(f'Decrypted message: {decrypted_msg}')
